59d05e86c72b6f941f77b9e32ab1072b18795fde
Nick Martello <thenickmartello@gmail.com>
2020-02-17 18:33:39 -0500

    mongoose model

---
+++ client/models/models.js
@@ -0,0 +1,33 @@
+let mongoose = require('mongoose');
+mongoose.connect("mongod://localhost/planner");
+
+  let scheduleSchema = new mongoose.Schema({
+    title: { 
+        type: String,
+        required: true
+    },
+    details:  {
+        type: String,
+        required: true
+    },
+    start: {
+        type: Number,
+    },
+    length: {
+        type: Number,
+    },
+    category: {
+        type: String,
+        required: true,
+    }
+  });
+
+let Schedule = mongoose.model("Schedule", scheduleSchema);
+
+Schedule
+
+// WORK 
+// EDUCATION
+// FUN
+// CHORES
+// SELFCARE
