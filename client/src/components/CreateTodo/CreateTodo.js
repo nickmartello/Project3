3f2a410ace94d6b3206b73ac87335abc8031b55e
Nick Martello <thenickmartello@gmail.com>
2020-03-03 23:16:11 -0500

    links working

--- client/src/components/CreateTodo/CreateTodo.js
+++ client/src/components/CreateTodo/CreateTodo.js
@@ -1,38 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 
-export default class CreateTodo extends Component {
+class CreateTodo extends Component {
+  state = {
+    todo_event: '',
+    todo_category: '',
+    todo_completed: false
+  }
 
-constructor(props){
-    super(props);
-
-    this.onChangeTodoEvent = this.onChangeTodoEvent.bind(this);
-    this.onChangeTodoCategory = this.onChangeTodoCategory.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-
-
-    this.state = {
-        todo_event: '',
-        todo_category: '',
-        todo_completed: false
-    }
-}
-
-onChangeTodoEvent(e) {
+  onChangeTodoEvent = e => {
     this.setState({
-        todo_event: e.target.value
+      todo_event: e.target.value
     });
-}
+  }
 
-onChangeTodoCategory(e) {
+  onChangeTodoCategory = e => {
     this.setState({
-        todo_category: e.target.value
+      todo_category: e.target.value
     });
-}
+  }
 
-
-onSubmit(e) {
+  onSubmit = e => {
     e.preventDefault();
 
     console.log(`Form Submitted`);
@@ -41,48 +29,50 @@ onSubmit(e) {
     console.log(`Todo Completed: ${this.state.todo_completed}`)
 
     const newTodo = {
-        todo_event: this.state.todo_event,
-        todo_category: this.state.todo_category,
-        todo_completed: this.state.todo_completed
+      todo_event: this.state.todo_event,
+      todo_category: this.state.todo_category,
+      todo_completed: this.state.todo_completed
     }
 
     axios.post('http://localhost:5001/todos/add', newTodo)
-        .then(res =>  console.log(res.data));
+      .then(res => console.log(res.data));
 
     this.setState({
-        todo_event: '',
-        todo_category: '',
-        todo_completed: false
+      todo_event: '',
+      todo_category: '',
+      todo_completed: false
 
     })
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: 10 }}>
+        <h3>Create New Todo</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Event: </label>
+            <input type="text"
+              className="form-control"
+              value={this.state.todo_event}
+              onChange={this.onChangeTodoEvent}
+            />
+          </div>
+          <div className="form-group">
+            <label>Category: </label>
+            <input type="text"
+              className="form-control"
+              value={this.state.todo_category}
+              onChange={this.onChangeTodoCategory}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create Todo" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-    render() {
-        return (
-            <div style={{marginTop: 10}}>
-                <h3>Create New Todo</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Event: </label>
-                        <input  type="text"
-                                className="form-control"
-                                value={this.state.todo_event}
-                                onChange={this.onChangeTodoEvent}
-                                />
-                    </div>
-                    <div className="form-group">
-                        <label>Category: </label>
-                        <input  type="text"
-                                className="form-control"
-                                value={this.state.todo_category}
-                                onChange={this.onChangeTodoCategory}
-                                />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Create Todo" className="btn btn-primary" />
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
+export default CreateTodo;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import "./Navbar.css"
-import { BrowserRouter as Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 class Navbar extends Component {
-    state = { 
-        dash: "active",
-        sched: "inactive",
-        activeComponent: "/"
-     }
+  state = {
+    dash: "active",
+    sched: "inactive",
+    activeComponent: "/"
+  }
 
-     dashClick = () =>{
-         this.setState({dash: "active", sched: "inactive", activeComponent: "/"})
-     }
-     schedClick = () =>{
-        this.setState({dash: "inactive", sched: "active", activeComponent: "/schedule"})
-    }
+  dashClick = () => {
+    this.setState({ dash: "active", sched: "inactive", activeComponent: "/" })
+  }
+  schedClick = () => {
+    this.setState({ dash: "inactive", sched: "active", activeComponent: "/schedule" })
+  }
 
-    render() { 
-        return (
-            <div className="navContainer">
-                <ul className="navUL">
-                    <li className="navLeft">
-                        <Link to="/settings">Settings</Link>
-                    </li>
-                    <li onClick={this.dashClick} className={this.state.dash}>
-                    Dashboard
-                    </li>
-                    <li onClick={this.schedClick} className={this.state.sched}>
-                    Schedule
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="navContainer">
+        <ul className="navUL">
+          <li className="navLeft">
+            <Link to="/settings">Settings</Link>
+          </li>
+          <li onClick={this.dashClick} className={this.state.dash}>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li onClick={this.schedClick} className={this.state.sched}>
+            <Link to="/schedule">Schedule</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 export default Navbar;
